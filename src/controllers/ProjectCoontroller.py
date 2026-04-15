82b5053bea8f9ce02a838506d63cde647738c4fa
Ahmed Hagras <ahmed31009749@f-eng.tanta.edu.eg>
2026-04-15 05:25:34 +0200

    add BaseController, DataController, and ProjectController with file handling and validation methods

---
+++ src/controllers/ProjectCoontroller.py
@@ -0,0 +1,18 @@
+import BaseController as BaseController
+from fastapi import UploadFile
+from helpers.config import get_settings
+from models import ResponseSignal
+import os
+
+class ProjectController(BaseController):
+    def __init__(self):
+        super().__init__()
+
+    def get_project_path(self, project_id: str):
+
+        project_dir = os.path.join(self.files_dir, project_id)
+
+        if not os.path.exists(project_dir):
+            os.makedirs(project_dir)
+        
+        return project_dir
