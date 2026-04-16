e65ac4797bc9aa8abb7f171f11ce2c46d166f30d
Ahmed Hagras <ahmed31009749@f-eng.tanta.edu.eg>
2026-04-16 07:07:28 +0200

    add MongoDB configuration settings to the Settings class

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,6 @@
 from .enums.ResponseEnums import ResponseSignal
 from .enums.ProcessingEnum import ProcessingEnum
+from .ProjectModel import ProjectModel
+from .ChunkModel import ChunkModel
+from .db_schemes.data_chunk import DataChunk
+from .db_schemes.project import Project
