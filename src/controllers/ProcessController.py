0e9ca644840742aac7f66f59880bf6120ba2f74d
Ahmed Hagras <ahmed31009749@f-eng.tanta.edu.eg>
2026-04-18 05:23:56 +0200

    add error handling for file loading in ProcessController

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -1,5 +1,6 @@
 from fastapi import status, HTTPException
 from fastapi.responses import JSONResponse
+from models.enums.ResponseEnums import ResponseSignal
 from .BaseController import BaseController
 from .ProjectController import ProjectController
 from langchain_community.document_loaders import TextLoader, PyMuPDFLoader
@@ -22,6 +23,13 @@ class ProcessController(BaseController):
     def get_file_loader(self, file_id: str):
         ## check if file exists from ProcessingEnum is txt or pdf to return the custom loader
         file_path = os.path.join(self.project_path, file_id)
+        if not os.path.exists(file_path):
+            return JSONResponse(
+                            status_code=status.HTTP_400_BAD_REQUEST,
+                            content={
+                                "signal": ResponseSignal.FILE_NOT_FOUND.value
+                            }
+                        )
         file_ext = self.get_file_extension(file_id=file_id)
         print(f"Processing file: {file_path} with extension: {file_ext}")
 
@@ -31,9 +39,11 @@ class ProcessController(BaseController):
             return PyMuPDFLoader(file_path)
         else:
             # use fastapi response to return error message with status code 400
-            raise HTTPException(
+            return JSONResponse(
                             status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=f"Unsupported file type: {file_ext}"
+                            content={
+                                "signal": ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
+                            }
                         )
         
     def get_file_content(self, file_id: str):
