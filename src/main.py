1d4101a745f892e4433823404eb23366a7439e2f
Ahmed Hagras <ahmed31009749@f-eng.tanta.edu.eg>
2026-04-19 00:47:44 +0200

    add logging and initialize LLMProviderFactory in startup event

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from routes import base, data
+from stores import LLMProviderFactory
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -12,9 +17,19 @@ async def startup_db_client():
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
+    llm_provider_factory = LLMProviderFactory(config=settings)
+
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
+
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE)
+
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongo_conn.close()
+    logger.info("MongoDB connection closed.")
+
 
 
 
