388213407cc9a1a178af67309e1152a4899c4c8d
Ahmed Hagras <ahmed31009749@f-eng.tanta.edu.eg>
2026-04-21 20:03:47 +0200

    refactor: update database connection logic for PostgreSQL integration

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from routes import base, data, nlp
 from stores import LLMProviderFactory, VectorDBProviderFactory
 from stores import TemplateParser
-
-from motor.motor_asyncio import AsyncIOMotorClient
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+# from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 import logging
 
@@ -15,9 +16,15 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup_span():
     settings = get_settings()
+    
     # Initialize your database client here
-    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    # app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    # app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+    postgres_conn = f"postgresql+asyncpg://{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+    app.db_engine = create_async_engine(postgres_conn)
+    app.db_client = sessionmaker(app.db_engine, class_=AsyncSession, 
+                                 expire_on_commit=False)
 
     llm_provider_factory = LLMProviderFactory(config=settings)
 
@@ -37,7 +44,7 @@ async def startup_span():
 
 @app.on_event("shutdown")
 async def shutdown_span():
-    app.mongo_conn.close()
+    # app.mongo_conn.close()
     app.vectordb_client.disconnect()
     logger.info("MongoDB connection closed.")
 
